cb54b8a3a38d84fb403e65c01369509ce05e8f53
micheledinelli <dinellimichele00@gmail.com>
2024-01-17 02:04:59 +0100

    feat: refactor apis

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import random
-from flask import Flask, send_file, Blueprint, request, jsonify
+import json
+import re
+
+import pandas as pd
+
+from flask import Flask, send_file, Blueprint, request, jsonify, make_response, Response
 from flask_cors import CORS 
 from flasgger import Swagger
-import pandas as pd
-import re
-from flask import Response
 
 app = Flask(__name__)
 CORS(app)
@@ -17,18 +19,19 @@ csv_directory = 'aculei-test.csv'
 image_names = []
 video_names = []
 
+global df 
+df = pd.read_csv(csv_directory, index_col=0)
+
 def load_video_names():
   global video_names
   video_names = [f for f in os.listdir(video_directory) if f.endswith('.webm')]
 
 def load_image_names():
   global image_names
-  image_names = [f for f in os.listdir(image_directory) if f.endswith('.jpg') or f.endswith('.jpeg')]
+  image_names = df['image_name']
 
 load_image_names()
 load_video_names()
-
-df = pd.read_csv(csv_directory)
 
 api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
@@ -80,38 +83,35 @@ def get_video():
 
 @api_v1.route('/image')
 def get_image():
-    if not image_names:
-      return 'No images found', 404
+  total_rows = len(df)
+  random_index = random.randint(0, total_rows - 1)
 
-    cam_number = request.args.get('cam')    
+  # Extract a random row based on the random index
+  random_row = df.iloc[random_index]
 
-    if cam_number != None:
-      return send_image_from_cam_filter(cam_number=cam_number)
-        
-    random_image_name = random.choice(image_names)
-    image_path = os.path.join(image_directory, random_image_name)
-    return send_file(image_path, mimetype='image/jpeg')
+  random_image_name = random_row['image_name']
+  image_path = os.path.join(image_directory, random_image_name)
 
-def send_image_from_cam_filter(cam_number):    
-    if cam_number is None:
-      return 'Please provide a valid cam number', 404
-        
-    cam_number = str(cam_number)
+  image_response = make_response(send_file(image_path, mimetype='image/jpg'))
 
-    try:
-      rows_with_cam_number = df[df['cam'] == cam_number]
-    
-      if len(rows_with_cam_number) == 0:
-        return f'Cam {cam_number} has no images at the moment', 404
-      
-      random_index = random.randint(0, len(rows_with_cam_number) - 1)
-      random_row = rows_with_cam_number.iloc[random_index]
-      image_name = random_row['image_name']
-      image_path = os.path.join(image_directory, image_name)      
-      
-      return send_file(image_path, mimetype='image/jpeg')
-    except IndexError:
-        return jsonify({'error': 'No row found for the given cam_number'})
+  image_response.headers['x-sha256'] = random_row['sha256']
+  image_response.headers['Access-Control-Expose-Headers'] = 'x-sha256'
+  return image_response
+
+@api_v1.route('/image-detail/<sha256>', methods=['GET'])
+def get_image_detail(sha256):
+  image_row = df[df['sha256'] == sha256]
+
+  if image_row.empty:
+    return "Image not found for the given SHA-256 value", 404
+
+  image_row['sha256'] = None
+  # Convert the row to a dictionary and then to JSON
+  image_data = image_row.to_dict(orient='records')
+
+  # Clean up the NaN values from the dictionary
+  cleaned_data = [{k: v for k, v in item.items() if pd.notnull(v)} for item in image_data]
+  return jsonify(cleaned_data[0])
 
 app.register_blueprint(api_v1)
 
