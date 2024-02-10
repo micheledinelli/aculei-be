00a53b09f6d3e56f0ac6c78b08b96e26fca28fb1
micheledinelli <dinellimichele00@gmail.com>
2024-02-10 19:51:59 +0100

    chore: added cors policy

--- app/archive/routes.py
+++ app/archive/routes.py
@@ -1,6 +1,7 @@
 import os
 import random
 import re
+import pandas as pd
 from flask import Blueprint, Response, request, send_file
 from flask import jsonify
 from app.db import df
@@ -28,9 +29,48 @@ def get_filters():
         description: Server error
     """
     try:
-        filters_labels = df.columns.tolist()
+        available_filters = [
+            "temperature",
+            "date_time",
+            "moon_phase",
+            "animal",
+            "hunter_camera"
+        ]
 
-        return jsonify({'filters': filters_labels})
+        filters_labels = list(filter(lambda x: x in available_filters, df.columns.tolist()))
+        
+        data = {}
+        for filter_label in filters_labels:
+            if filter_label == "temperature":
+                min_max = (int(df.temperature.min()), 
+                           int(df.temperature.max()))
+                
+                pairs = list()
+                for i in range(min_max[0], min_max[1], 5):
+                    pairs.append({'min': i, 'max': i + 5})
+                data[filter_label] = {'spec': pairs}
+
+            elif filter_label == "moon_phase":
+                data[filter_label] = {'spec': list(filter(lambda x: not pd.isna(x), df.moon_phase.unique()))}
+
+            elif filter_label == "animal":
+                data[filter_label] = {'spec': list(filter(lambda x: not pd.isna(x), df.animal.unique()))}
+
+            elif filter_label == "date_time":
+                dates = pd.to_datetime(df.date_time)
+                min = dates.min().date()
+                max = dates.max().date()            
+
+                data['date'] = {'spec': {'min': min, 'max': max}}
+
+            elif filter_label == "hunter_camera":
+                hunter_cameras = list(filter(lambda x: not pd.isna(x), df.hunter_camera.unique()))
+                hunter_cameras = sorted(hunter_cameras)
+                data[filter_label] = {'spec': hunter_cameras}
+            else:
+                data[filter_label] = None
+
+        return jsonify({'filters': data})
     except Exception as e:
         logger.exception('An exception occurred during a request.', str(e))
         return jsonify({'error': 'error in getting filters'})
